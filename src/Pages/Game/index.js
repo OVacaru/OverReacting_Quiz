2bf50f6cf05ac390f3610fd2e43d9c6fae267593
Stylianos Thomas <stylianos.thomas@gmail.com>
2020-12-08 15:12:35 +0000

    refactured our code for best readability

--- src/Pages/Game/index.js
+++ src/Pages/Game/index.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react'
-//import { Link } from 'react-router-dom';
-import Question from '../../Components/Question'
-import Results from '../Results'
+import React, { Component } from 'react';
+import Player from '../../Components/Player';
+import Question from '../../Components/Question';
+import Results from '../Results';
 import { connect } from 'react-redux';
 
 class Game extends Component {
 
     render() {
-        console.log("Game Page has been called")
         return (
             <>
+                <p>This is the page: Game</p>
                 {this.props.questions.length > 0 && this.props.questionID === this.props.questions.length ? 
-                    <Results /> :
-                    <Question question={this.props.questions[this.props.questionID]}/>
+                    <Results winner={this.props.players}/> :
+                    <div id="gameArea">
+                        <Player player={this.props.players}/>
+                        <Question question={this.props.questions[this.props.questionID]}/>
+                    </div>
                 }
             </>
-        )
-    }
-}
+        );
+    };
+};
 
 export const mSTP = state => ({
     totalPlayers: state.totalPlayers,
     players: state.players,
     questionID: state.questionID,
     questions: state.questions
-})
+});
 
-export default connect(mSTP, {Question} )(Game);
+export default connect(mSTP, { Question , Results } )(Game);
--- src/Pages/Options/index.js
+++ src/Pages/Options/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom' ;
 import { getQuestions } from "../../Actions";
 import { setPlayers } from "../../Actions";
@@ -8,57 +8,55 @@ class Options extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        this.props.setPlayers(this.state.totalPlayers)
+        this.props.setPlayers(this.state.totalPlayers);
         this.props.getQuestions(this.state.questions, this.state.category, this.state.difficulty);
     };
 
     handleChange = e => {
-        const noOfPlayers = parseInt(e.target.form.noOfPlayers.value)
-        const noOfQuestions = e.target.form.noOfQuestions.value
-        const difficulty = e.target.form.difficulty.value
-        const category = e.target.form.category.value
-        this.setState({ totalPlayers: noOfPlayers, questions: noOfQuestions, category:category, difficulty: difficulty })
+        const noOfPlayers = parseInt(e.target.form.noOfPlayers.value);
+        const noOfQuestions = e.target.form.noOfQuestions.value;
+        const difficulty = e.target.form.difficulty.value;
+        const category = e.target.form.category.value;
+        this.setState({ totalPlayers: noOfPlayers, questions: noOfQuestions, category:category, difficulty: difficulty });
     }
 
     render() {
-
-        console.log("Option Page has been called")
         return (
-        <>
-        <h3> Form goes here </h3>
-        <form onSubmit={this.handleSubmit}>
-            <label htmlFor="noOfPlayers"> How Many Players? </label>
-            <input type="number" name="noOfPlayers" min="1" max="4" />
+        <div id="gameSettings">
+            <p>This is the page: Options</p>
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="noOfPlayers"> How Many Players? </label>
+                <input type="number" name="noOfPlayers" min="1" max="4" />
+                <br/>
+                <label htmlFor="noOfQuestions"> How Many Questions? </label>
+                <input type="number" name="noOfQuestions" min="1" max="20"/>
+                <br/>
+                <label htmlFor="category"> Choose a category: </label>
+                <select name="category" >
+                    <option value="9" > General Knowledge </option>
+                    <option value="10" > Books </option>
+                    <option value="11" > Film </option>
+                    <option value="14" > Television</option>
+                    <option value="15" > Video Games </option>
+                    <option value="17" > Science and Nature </option>
+                    <option value="22" > Geography </option>
+                    <option value="23" > History </option>
+                </select>
+                <br/>
+                <label htmlFor="difficulty"> Choose a Difficulty Level: </label>
+                <select name="difficulty">
+                    <option value="easy"> Easy </option>
+                    <option value="medium"> Medium </option>
+                    <option value="hard"> Hard </option>
+                </select>
+                <br/>
+                <input type="submit" value="Confirm Options" onClick= {this.handleChange}/>
+            </form>
             <br/>
-            <label htmlFor="noOfQuestions"> How Many Questions? </label>
-            <input type="number" name="noOfQuestions" min="1" max="20"/>
-            <br/>
-            <label htmlFor="category"> Choose a category: </label>
-            <select name="category" >
-                <option value="9" > General Knowledge </option>
-                <option value="10" > Books </option>
-                <option value="11" > Film </option>
-                <option value="14" > Television</option>
-                <option value="15" > Video Games </option>
-                <option value="17" > Science and Nature </option>
-                <option value="22" > Geography </option>
-                <option value="23" > History </option>
-            </select>
-            <br/>
-            <label htmlFor="difficulty"> Choose a Difficulty Level: </label>
-            <select name="difficulty">
-                <option value="easy"> Easy </option>
-                <option value="medium"> Medium </option>
-                <option value="hard"> Hard </option>
-            </select>
-            <br/>
-            <input type="submit" value="Confirm Options" onClick= {this.handleChange}/>
-        </form>
+            <Link to='/game'>START PLAYING</Link>
+        </div>
+        );
+    };
+};
 
-        <Link to='/game'>START PLAYING</Link>
-        </>
-        )
-    }
-}
-
-export default connect (null, { getQuestions, setPlayers })(Options);
+export default connect (null, { getQuestions, setPlayers } )(Options);
