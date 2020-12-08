fa7fe4c94ac818f8d994a3395e07089cd3df488c
Stylianos Thomas <stylianos.thomas@gmail.com>
2020-12-08 12:54:54 +0000

    added choice for player number, categories and multiple choice questions

--- src/Components/Question/index.js
+++ src/Components/Question/index.js
@@ -5,7 +5,7 @@ import { mSTP } from '../../Pages/Game';
 
 class Question extends Component {
 
-    handleChange = e => {
+    handleBoolean = e => {
         e.preventDefault();
         const userAnswer = e.target.value
         const correctAnswer = this.props.question.correct_answer;
@@ -18,19 +18,58 @@ class Question extends Component {
             this.props.nextQuestion();
         };
     };
+
+    handleMultiple = e => {
+        e.preventDefault();
+        const userAnswer = e.target.value
+        const correctAnswer = this.props.question.correct_answer;
+        if (userAnswer === correctAnswer) {
+            console.log( "You got it right!")
+            this.props.nextQuestion();
+            
+        } else {
+            console.log( "You got it wrong!")
+            this.props.nextQuestion();
+        };
+    };
+
+    shuffleArray (array) {
+        let currentId = array.length;
+        while (0 != currentId) {
+            let randomId = Math.floor(Math.random()* currentId);
+            currentId -= 1;
+            let temp = array[currentId];
+            array[currentId] = array[randomId];
+            array[randomId] = temp;
+        }
+        return array;
+    }
     
     render() {
-        const showQuestion = this.props.question.question;
         console.log("Question Component has been called");
-
+        const showQuestion = this.props.question.question;
+        const allAnswers = [
+            this.props.question.correct_answer,
+            this.props.question.incorrect_answers[0],
+            this.props.question.incorrect_answers[1],
+            this.props.question.incorrect_answers[2]]
+        const randomAnswer = this.shuffleArray(allAnswers);
         return (
             <>
                 <h1>Game</h1>
-                <form>
-                    <h1>{showQuestion}</h1> <br/>
-                    <input type="submit" value="True" onClick= {this.handleChange}/> <br/>
-                    <input type="submit" value="False" onClick= {this.handleChange}/> <br/>
-                </form>
+                <h1>{showQuestion}</h1> <br/>
+                {this.props.question.type === "boolean" ? 
+                    <form>
+                        <input type="submit" value="True" onClick= {this.handleBoolean}/> <br/>
+                        <input type="submit" value="False" onClick= {this.handleBoolean}/> <br/>
+                    </form> :
+                    <form>
+                        <input type="submit" value={randomAnswer[0]} onClick= {this.handleMultiple}/> <br/>
+                        <input type="submit" value={randomAnswer[1]} onClick= {this.handleMultiple}/> <br/>
+                        <input type="submit" value={randomAnswer[2]} onClick= {this.handleMultiple}/> <br/>
+                        <input type="submit" value={randomAnswer[3]} onClick= {this.handleMultiple}/> <br/>
+                    </form>
+                }
             </>
         )
     }
--- src/Pages/Options/index.js
+++ src/Pages/Options/index.js
@@ -1,29 +1,49 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom' ;
 import { getQuestions } from "../../Actions";
+import { setPlayers } from "../../Actions";
 import { connect } from 'react-redux';
 
 class Options extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        this.props.getQuestions(this.state.questions, this.state.difficulty);
+        this.props.setPlayers(this.state.totalPlayers)
+        this.props.getQuestions(this.state.questions, this.state.category, this.state.difficulty);
     };
 
     handleChange = e => {
+        const noOfPlayers = parseInt(e.target.form.noOfPlayers.value)
         const noOfQuestions = e.target.form.noOfQuestions.value
         const difficulty = e.target.form.difficulty.value
-        this.setState({ questions: noOfQuestions, difficulty: difficulty })
+        const category = e.target.form.category.value
+        this.setState({ totalPlayers: noOfPlayers, questions: noOfQuestions, category:category, difficulty: difficulty })
     }
 
     render() {
+
         console.log("Option Page has been called")
         return (
         <>
         <h3> Form goes here </h3>
         <form onSubmit={this.handleSubmit}>
+            <label htmlFor="noOfPlayers"> How Many Players? </label>
+            <input type="number" name="noOfPlayers" min="1" max="4" />
+            <br/>
             <label htmlFor="noOfQuestions"> How Many Questions? </label>
-            <input type="number" name="noOfQuestions" min="1" max="50" value={this.questions}/>          
+            <input type="number" name="noOfQuestions" min="1" max="20"/>
+            <br/>
+            <label htmlFor="category"> Choose a category: </label>
+            <select name="category" >
+                <option value="9" > General Knowledge </option>
+                <option value="10" > Books </option>
+                <option value="11" > Film </option>
+                <option value="14" > Television</option>
+                <option value="15" > Video Games </option>
+                <option value="17" > Science and Nature </option>
+                <option value="22" > Geography </option>
+                <option value="23" > History </option>
+            </select>
             <br/>
             <label htmlFor="difficulty"> Choose a Difficulty Level: </label>
             <select name="difficulty">
@@ -41,20 +61,4 @@ class Options extends Component {
     }
 }
 
-export default connect (null, { getQuestions })(Options);
-
-
-{/* <label for="category">Choose a Category</label>
-            <select name="category">
-                <option value="geography">Geography</option>
-                <option value="science">Science</option>
-                <option value="entertainment">Entertainment</option>
-                <option value="general">General Knowledge</option>
-            </select> */}
-
- {/* <label for="type">Type of Questions?</label>
-                // true or false or multiple choice
-            <select>
-                <option value="trueOrFalse">True or False</option>
-                <option value="multipleChoice">Multiple Choice</option>
-            </select> */}
+export default connect (null, { getQuestions, setPlayers })(Options);
