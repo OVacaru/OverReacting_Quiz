bf244c15397b7f8759cdfabbc960f2ea0d01ce1c
Olivia <olivia.vacaru@gmail.com>
2020-12-09 12:36:43 +0000

    kai's test are passing now in Options, but need for more test to test handleSubmit

--- src/Pages/Options/Options.test.js
+++ src/Pages/Options/Options.test.js
@@ -1,6 +1,6 @@
 import Options from './index';
 import { shallow } from 'enzyme';
-import { setPlayers } from '../../Actions';
+
 
 describe('Options', () => {
     let wrapper, form, inputs, selects, numInput, initProps, newProps, getQuestionMock, setPlayerMock;
@@ -19,6 +19,8 @@ describe('Options', () => {
         wrapper = shallow(<Options.WrappedComponent {...stub} getQuestionMock={getQuestionMock} setPlayerMock={setPlayerMock} />)
         form = wrapper.find('form');
     });
+
+
 
     test('it renders a form with three inputs', () => {
         expect(form).toHaveLength(1);
@@ -41,18 +43,19 @@ describe('Options', () => {
     });
 
     test('it updates player Prop on user input', () => {
+        form = wrapper.find('form');
         numInput = form.find('input').first();
         initProps = wrapper.prop('totalPlayers');
         numInput.simulate('change', { target: { value: 5 } })
-        newProps = wrapper.prop('totalPlayers');
+        newProps = wrapper.props('totalPlayers');
         expect(newProps).not.toEqual(initProps)
     });
 
     test('it updates question Prop on user input', () => {
-        numInput = form.find('input').second();
+        numInput = form.find('input').first();
         initProps = wrapper.prop('noOfQuestions');
         numInput.simulate('change', { target: { value: 5 } })
-        newProps = wrapper.prop('noOfQuestions');
+        newProps = wrapper.props('noOfQuestions');
         expect(newProps).not.toEqual(initProps)
     });
 
@@ -60,7 +63,7 @@ describe('Options', () => {
         numInput = form.find('select').first();
         initProps = wrapper.prop('category');
         numInput.simulate('change', { target: { value: "geography" } })
-        newProps = wrapper.prop('category');
+        newProps = wrapper.props('category');
         expect(newProps).not.toEqual(initProps)
     });
 
@@ -68,17 +71,19 @@ describe('Options', () => {
         numInput = form.find('select').first();
         initProps = wrapper.prop('difficulty');
         numInput.simulate('change', { target: { value: "hard" } })
-        newProps = wrapper.prop('difficulty');
+        newProps = wrapper.props('difficulty');
         expect(newProps).not.toEqual(initProps)
     });
+
 
     // test('it calls on getQuestion prop on form submission', () => {
     //     form = wrapper.find('form');
     //     wrapper.setProp({ noOfQuestion: '5'})
     // })
-
+    
     test('it has 1 Link', () => {
         let links = wrapper.find('Link');
         expect(links).toHaveLength(1)
     })
 })
+
