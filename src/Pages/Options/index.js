f9221a4e84637b8f928e323f665384c65d418591
stephanie-ai <stephanie_cooper@hotmail.com>
2020-12-09 10:15:56 +0000

    add styling to options page

--- src/Pages/Options/index.js
+++ src/Pages/Options/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom' ;
 import { getQuestions } from "../../Actions";
 import { setPlayers } from "../../Actions";
 import { connect } from 'react-redux';
+import './style.css';
 
 class Options extends Component {
 
@@ -24,36 +25,45 @@ class Options extends Component {
         return (
         <div id="optionPage">
             <p>This is the page: Options</p>
+            <div id="optionsForm">
             <form onSubmit={this.handleSubmit}>
-                <label htmlFor="noOfPlayers"> How Many Players? </label>
-                <input type="number" name="noOfPlayers" min="1" max="4" />
-                <br/>
-                <label htmlFor="noOfQuestions"> How Many Questions? </label>
-                <input type="number" name="noOfQuestions" min="1" max="20"/>
-                <br/>
-                <label htmlFor="category"> Choose a category: </label>
-                <select name="category" >
-                    <option value="9" > General Knowledge </option>
-                    <option value="10" > Books </option>
-                    <option value="11" > Film </option>
-                    <option value="14" > Television</option>
-                    <option value="15" > Video Games </option>
-                    <option value="17" > Science and Nature </option>
-                    <option value="22" > Geography </option>
-                    <option value="23" > History </option>
-                </select>
-                <br/>
-                <label htmlFor="difficulty"> Choose a Difficulty Level: </label>
-                <select name="difficulty">
-                    <option value="easy"> Easy </option>
-                    <option value="medium"> Medium </option>
-                    <option value="hard"> Hard </option>
-                </select>
-                <br/>
-                <input type="submit" value="Confirm Options" onClick= {this.handleChange}/>
+                <div className="formLine">
+                    <label htmlFor="noOfPlayers"> How Many Players? </label>
+                    <input type="number" name="noOfPlayers" min="1" max="4" />
+                </div>
+                <div className="formLine">
+                    <label htmlFor="noOfQuestions"> How Many Questions? </label>
+                    <input type="number" name="noOfQuestions" min="1" max="20"/>
+                </div>
+                <div className="formLine">
+                    <label htmlFor="category"> Choose a Category: </label>
+                    <select name="category" >
+                        <option value="9" > General Knowledge </option>
+                        <option value="10" > Books </option>
+                        <option value="11" > Film </option>
+                        <option value="14" > Television</option>
+                        <option value="15" > Video Games </option>
+                        <option value="17" > Science and Nature </option>
+                        <option value="22" > Geography </option>
+                        <option value="23" > History </option>
+                    </select>
+                </div>
+                <div className="formLine">
+                    <label htmlFor="difficulty"> Choose a Difficulty Level: </label>
+                    <select name="difficulty">
+                        <option value="easy"> Easy </option>
+                        <option value="medium"> Medium </option>
+                        <option value="hard"> Hard </option>
+                    </select>
+                </div>
+                <div className="formLine">
+                    <input type="submit" className="optionsBtn" value="Confirm Options" onClick= {this.handleChange}/>
+                </div>
+                
             </form>
+            </div>
             <br/>
-            <Link to='/game'>START PLAYING</Link>
+            <Link to='/game' className="startBtn">START PLAYING</Link>
         </div>
         );
     };
